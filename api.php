a5e27e31a766ed72a303b8c5ad502f4e14c1515e
LiShiYv <qq15354304250@163.com.com>
2019-03-13 10:01:12 +0800

    first commit

---
+++ api.php
@@ -0,0 +1,14 @@
+<?php
+$web_server =[
+    '192.168.85.139',
+    '192.168.85.167'
+];
+$cmd = "cd / && ls -l";
+$res = shell_exec($cmd);
+echo $res;echo '<hr>';die;
+foreach($web_server as $k=>$v){
+    $cmd = 'ssh'.$v.'"cd /data/wwwroot/default/shop/api; git pull" ';
+    echo $cmd;echo '</br>';
+}
+
+?>
