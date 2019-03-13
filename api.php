9693f8ad645bcf78700b069b27243bb4e36b2026
LiShiYv <qq15354304250@163.com.com>
2019-03-13 10:59:11 +0800

    666667

--- api.php
+++ api.php
@@ -7,9 +7,9 @@ $web_server =[
     '192.168.85.139',
     '192.168.85.167'
 ];
-$cmd = "cd / && ls -l";
-$res = shell_exec($cmd);
-echo $res;echo '<hr>';
+//$cmd = "cd / && ls -l";
+//$res = shell_exec($cmd);
+//echo $res;echo '<hr>';
 foreach($web_server as $k=>$v){
     $cmd = 'ssh'.$v.'"cd /data/wwwroot/default/shop/api; git pull" ';
 	echo $cmd;echo '</br>';
